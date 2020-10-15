bb3e2a4616477a2c4147701994bccbeb1256e523
Juan Luis Millan Hidalgo <juanluismillanhidalgo@gmail.com>
2020-10-15 17:40:12 +0200

    fotos

--- updategit.sh
+++ updategit.sh
@@ -3,6 +3,17 @@
 echo "Bienvenido al actualizador de git de Juan Luis Millán."
 echo "Introduce los cambios realizados(commit):"; read input
 
+#Actualizamos los ficheros modificados.
+bundle exec jekyll build --incremental
+
+#Copiamos los html creados en nuestro _site y los movemos a nuestro repositorio que esta enlazado a netlify
+cp -r /home/juanlu/GitHub/blog/juanlumillan-blog/_site/* /home/juanlu/GitHub/blog/juanlumillan-netlify/
+
+
+#Accedemos al repostirio donde están creadas nuestros html.
+cd /home/juanlu/GitHub/blog/juanlumillan-netlify/
+
+#Agreamos los cambios realizados con git,hacemos el commit y lo subimos con un push.
 git add *
 
 while [$input = ""]
@@ -14,4 +25,4 @@ done
 git commit -am "$input"
 git push
 
-exit
+exit 0
